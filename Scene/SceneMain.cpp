47128323101eb20b74152d6bcb3f09476d86205c
Hmmtrnt <rento5310@gmail.com>
2022-11-24 12:26:35 +0900

    SceneMain追加

---
+++ Scene/SceneMain.cpp
@@ -0,0 +1,36 @@
+#include "SceneMain.h"
+#include "DxLib.h"
+
+SceneMain::SceneMain()
+{
+
+}
+
+SceneMain::~SceneMain()
+{
+
+}
+
+// ‰Šú‰»
+void SceneMain::init()
+{
+
+}
+
+// I—¹ˆ—
+void SceneMain::end()
+{
+
+}
+
+// XVˆ—
+void SceneMain::update()
+{
+
+}
+
+// •`‰æˆ—
+void SceneMain::draw()
+{
+	DrawString(0, 0, "SceneMain", GetColor(255, 255, 255));
+}
---
+++ Scene/SceneMain.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "SceneBase.h"
+class SceneMain : public SceneBase
+{
+public:
+	SceneMain();
+	virtual ~SceneMain();
+
+	virtual void init();		// ‰Šú‰»
+	virtual void end();			// I—¹ˆ—
+	virtual void update();		// XVˆ—
+	virtual void draw();		// •`‰æˆ—
+
+private:
+
+};
