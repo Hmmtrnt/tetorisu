8990add05d99244d4162f6055f8e46cc10514367
Hmmtrnt <rento5310@gmail.com>
2022-11-29 13:37:43 +0900

    ObjectBase追加

---
+++ Object/ObjectBase.cpp
@@ -0,0 +1,32 @@
+#include "ObjectBase.h"
+#include "DxLib.h"
+
+ObjectBase::ObjectBase()
+{
+
+}
+
+ObjectBase::~ObjectBase()
+{
+
+}
+
+void ObjectBase::Init()
+{
+
+}
+
+void ObjectBase::End()
+{
+
+}
+
+void ObjectBase::Update()
+{
+
+}
+
+void ObjectBase::Draw()
+{
+
+}
---
+++ Object/ObjectBase.h
@@ -0,0 +1,18 @@
+#pragma once
+class ObjectBase
+{
+public:
+	ObjectBase();
+	virtual ~ObjectBase();
+
+	// ‰Šú‰»ˆ—
+	void Init();
+	// I—¹ˆ—
+	void End();
+	// XVˆ—
+	void Update();
+	// •`‰æˆ—
+	void Draw();
+private:
+	
+};
